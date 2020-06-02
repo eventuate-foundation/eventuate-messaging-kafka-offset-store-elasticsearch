22dd92db8d3f4d5c7cce064b7b6eec8e3be94e87
Chris Richardson <chris@chrisrichardson.net>
2020-06-02 12:58:15 -0700

    Attempt to fix java.net.ConnectException: Connection refused in CircleCI

---
+++ .circleci/setenv-circle-ci.sh
@@ -0,0 +1,10 @@
+
+
+# Host DNS name doesn't resolve in Docker alpine images
+export DOCKER_HOST_IP=$(hostname -I | sed -e 's/ .*//g')
+
+export TERM=dumb
+
+export SDKMAN_DIR=/home/circleci/.sdkman
+
+source "$SDKMAN_DIR/bin/sdkman-init.sh"
